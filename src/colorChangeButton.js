aa962965a45847dffa776d3951aa148969a5edd3
Shreya <shreyashah115@gmail.com>
2020-12-22 16:18:07 +0530

    Color switcher

---
+++ src/colorChangeButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ColorChangeButton (props) {
+    console.log(props);
+  return (
+    <button className={props.color} onClick={() => props.setColor(props.color)}>{props.color}</button>
+  )
+}
+
+export default ColorChangeButton
